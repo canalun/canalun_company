473d0c805e83474c22592a138c6e0d7b3d204efd
canalun <kanakanarunrun@gmail.com>
2021-12-27 02:03:32 +0900

    mod: toppage image

--- contents/index.js
+++
@@ -1,5 +0,0 @@
-const imageArea = document.getElementById('imageArea');
-const images = ['./images/CEOs/boy.jpeg', './images/CEOs/dragon.png'];
-
-const imageNo = Math.floor( Math.random() * images.length)
-imageArea.src = images[imageNo];
