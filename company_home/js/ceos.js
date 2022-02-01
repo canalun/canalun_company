804de47f6516bcc6403083692655ec01ff7eaa67
canalun <kanakanarunrun@gmail.com>
2022-02-01 11:06:40 +0900

    developping entry js script

--- company_home/js/ceos.js
+++ company_home/js/ceos.js
@@ -3,8 +3,6 @@ const images = ['./images/CEOs/boy.jpeg',
                 './images/CEOs/alpaca.jpeg',
                 './images/CEOs/girrafe.jpeg',
                 './images/CEOs/goat.jpeg'];
-
 const imageNo = Math.floor( Math.random() * images.length);
 imageSrc = images[imageNo];
-console.log(imageNo, imageSrc)
 document.write('<img src="'+imageSrc+'" width="395" height="395">');
--- company_home/js/entries.js
+++ company_home/js/entries.js
@@ -1,2 +1,19 @@
-const entryList = require('../entry_list/Hatena.json');
-console.log(entryList);
+// fetch("../entry_list/Hatena.json")
+// .then(response => {
+//     return response.json();
+// })
+// .then(jsondata => console.log(jsondata));
+
+
+// const jsonData= require("../entry_list/Hatena.json");
+// console.log(jsonData);
+
+
+import { readFileSync } from 'fs';
+
+const jsonObject = JSON.parse(readFileSync('../entry_list/Hatena.json', 'utf8'));
+const result = {};
+
+jsonObject.forEach((obj) => {
+    console.log(obj)
+});
